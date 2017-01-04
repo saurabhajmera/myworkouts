22777a0fd7ce9855ef02d6a3abf22ce8d4945db9
Saurabh Ajmera <sajmera@live.com>
2017-01-04 14:03:37 -0800

    initial commit

---
+++ src/pages/workouts/workouts.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by sajmera on 1/4/17.
+ */
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'workouts',
+    templateUrl: 'workouts.html'
+})
+export class WorkoutsPage {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+}
